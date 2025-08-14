0c69c19b4b9110309a054b192f02f51d3f939863
Syyed <sayyedanas142@gmail.com>
2025-08-14 08:06:57 +0530

    Frontend Url Changed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://resumebuilder-qaby.onrender.com",
     credentials: true,
   })
 );
